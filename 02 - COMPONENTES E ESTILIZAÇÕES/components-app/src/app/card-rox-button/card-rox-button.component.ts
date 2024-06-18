f6ad24c1a43021cf9b508f65fae907103889ec3b
Filipe Lima <fhemrique55@gmail.com>
2024-06-18 16:29:32 -0300

    Template Dentro do Componente (prop "template")

--- 02 - COMPONENTES E ESTILIZAÇÕES/components-app/src/app/card-rox-button/card-rox-button.component.ts
+++ 02 - COMPONENTES E ESTILIZAÇÕES/components-app/src/app/card-rox-button/card-rox-button.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-rox-button',
-  templateUrl: './card-rox-button.component.html',
+  /* templateUrl: './card-rox-button.component.html',*/
+  template: `
+  <div class="card-roxo-button">Adquirir</div>
+  ` ,
   styleUrl: './card-rox-button.component.scss'
 })
 export class CardRoxButtonComponent {
