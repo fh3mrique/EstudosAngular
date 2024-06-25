21bff9d82b3fb7cd5b7368e44a50b22f8274a182
Filipe Lima <fhemrique55@gmail.com>
2024-06-25 09:36:27 -0300

    Introdução aos Pipes

--- 03 - BINDING, DIRETIVAS, TEMPLATES E DECORATORS/Introdução aos Pipes/intro-pipe-app/src/app/app.component.ts
+++ 03 - BINDING, DIRETIVAS, TEMPLATES E DECORATORS/Introdução aos Pipes/intro-pipe-app/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  text: string = 'Filipe';
+  pessoa = {
+    name: 'Theo',
+    age: 43
+  }
 
+  client = {
+    name: 'Joaquin',
+    status: 1
+  }
+  client2 = {
+    name: 'Marge',
+    status: 2
+  }
+  client3 = {
+    name: 'Pedro',
+    status: 3
+  }
 }
