fa925278852294f3cc86cc0be8f85c0df7ca45f0
Filipe Lima <fhemrique55@gmail.com>
2024-06-26 17:31:29 -0300

    Criação do Layout do Componente de Detalhes

--- 04 - PROJETO - filtro de listas de usuarios/users-filter-project/src/app/angular-material/angular-material.module.ts
+++ 04 - PROJETO - filtro de listas de usuarios/users-filter-project/src/app/angular-material/angular-material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-    imports:[MatButtonModule, MatIconModule],
-    exports:[MatButtonModule, MatIconModule]
+    imports: [MatButtonModule, MatIconModule, MatListModule, MatDividerModule],
+    exports: [MatButtonModule, MatIconModule, MatListModule, MatDividerModule]
 })
-export class AngularMaterialModule {}
+export class AngularMaterialModule { }
