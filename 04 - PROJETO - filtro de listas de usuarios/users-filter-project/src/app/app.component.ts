1e399a9e2deb508d2ccb816396f92419b36eb901
Filipe Lima <fhemrique55@gmail.com>
2024-06-27 20:34:41 -0300

    Criação do Evento de Click no Usuário

--- 04 - PROJETO - filtro de listas de usuarios/users-filter-project/src/app/app.component.ts
+++ 04 - PROJETO - filtro de listas de usuarios/users-filter-project/src/app/app.component.ts
@@ -9,5 +9,12 @@ import { UsersList } from './data/users-list';
 })
 export class AppComponent {
 
-  userSelected: IUser = UsersList[0];
+  userSelected: IUser = {} as IUser;
+
+  showUserDetails: boolean = false;
+
+  onUserSelected(user: IUser){
+    this.userSelected = user;
+    this.showUserDetails = true;
+  }
 }
