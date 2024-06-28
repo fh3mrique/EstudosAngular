589af555e5c1d773b90af5c6874194843fc97153
Filipe Lima <fhemrique55@gmail.com>
2024-06-28 20:33:25 -0300

    Acessar Elemento HTML no Componente com "@ViewChild”

--- 05 - DIRETIVAS, TEMPLATEWS E DECORATORS DINÂMICOS/PROJETO/src/app/app.component.ts
+++ 05 - DIRETIVAS, TEMPLATEWS E DECORATORS DINÂMICOS/PROJETO/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +7,23 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+
+  @ViewChild("meuInput")
+  meuInputEl!: ElementRef<HTMLInputElement>;
+
+  @ViewChild("minhaDiv")
+  minhaDivEL!: ElementRef<HTMLDivElement>;
   
-  clicou(input: HTMLInputElement){
-    input.value = 'Atualizado'
+  updateInputText(){
+    this.meuInputEl.nativeElement.value = 'Texto atualizado'
+  }
+
+  focus(){
+    this.meuInputEl.nativeElement.focus()
+  }
+
+  updateDivContent(){
+    this.minhaDivEL.nativeElement.textContent = "Conteudo atualizado"
   }
 
 
