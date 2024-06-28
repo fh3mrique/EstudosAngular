efbf9565d535e05bdd557a5ecf09889301b892c2
Filipe Lima <fhemrique55@gmail.com>
2024-06-28 20:04:33 -0300

    Template Variables: Criação e Change Detection

--- 05 - DIRETIVAS, TEMPLATEWS E DECORATORS DINÂMICOS/PROJETO/src/app/app.component.ts
+++ 05 - DIRETIVAS, TEMPLATEWS E DECORATORS DINÂMICOS/PROJETO/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'PROJETO';
+  constructor(
+    private _cdRef: ChangeDetectorRef
+  ){}
+
+  chargeDetection(){
+    this._cdRef.detectChanges()
+  }
 }
