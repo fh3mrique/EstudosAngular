d7bf28837d6f008c4a4da42909cc4365e2dc4d84
Filipe Lima <fhemrique55@gmail.com>
2024-08-09 20:07:44 -0300

    "ng-container" vs "ng-template"

--- 07 - TOPICOS AVANÇADOS PROJEÇÃO DE CONTEUDO/exaple/src/app/app.module.ts
+++ 07 - TOPICOS AVANÇADOS PROJEÇÃO DE CONTEUDO/exaple/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuCompComponent } from './meu-comp/meu-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuCompComponent
   ],
   imports: [
     BrowserModule,
