0b183f0b8b9d59c570d3c1d2b24f9ded53fce84e
Filipe Lima <fhemrique55@gmail.com>
2024-08-09 21:24:43 -0300

    Projetando uma Tag de "ng-template" em outro Componente

--- 07 - TOPICOS AVANÇADOS PROJEÇÃO DE CONTEUDO/exaple/src/app/card/card.component.ts
+++ 07 - TOPICOS AVANÇADOS PROJEÇÃO DE CONTEUDO/exaple/src/app/card/card.component.ts
@@ -1,20 +1,12 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 
 @Component({
-  selector: 'app-card',  // Define o seletor que será usado para inserir este componente no HTML
-  templateUrl: './card.component.html',  // Referencia o arquivo HTML que define o layout do componente
-  styleUrls: ['./card.component.scss']  // Referencia o arquivo de estilo que define a aparência do componente
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-    // Define uma propriedade de entrada chamada 'header' que recebe um template (obrigatório)
-    @Input({required: true})
-    header!: TemplateRef<any>;
-
-    // Define uma propriedade de entrada chamada 'content' que recebe um template (obrigatório)
-    @Input({required: true})
-    content!: TemplateRef<any>;
-
-    // Define uma propriedade de entrada chamada 'footer' que recebe um template (obrigatório)
-    @Input({required: true})
-    footer!: TemplateRef<any>;
+   // Captura o template com o identificador 'meuTemplate' que é passado para o componente
+   @ContentChild('meuTemplate')
+   content!: TemplateRef<any>;
 }
