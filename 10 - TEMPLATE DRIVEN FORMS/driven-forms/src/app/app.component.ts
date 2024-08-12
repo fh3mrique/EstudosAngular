7b4f823d9cf3ba4fb3ce982bd65bb97980c3b4de
Filipe Lima <fhemrique55@gmail.com>
2024-08-12 16:34:17 -0300

    Acessar a Instância do FormControl dentro da Classe do Componente

--- 10 - TEMPLATE DRIVEN FORMS/driven-forms/src/app/app.component.ts
+++ 10 - TEMPLATE DRIVEN FORMS/driven-forms/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  nome: string = 'filipe'
+export class AppComponent implements AfterViewInit {
+  // Usando ViewChild para acessar a instância do NgModel associado ao input
+  @ViewChild('meuInputFormControl') 
+  inputEL!: NgModel;  // A instância do NgModel será atribuída aqui após a view ser inicializada
 
-  onChange(text: string){
-    console.log(text)
+  // Hook que é executado após a inicialização completa da view do componente
+  ngAfterViewInit(): void {
+    console.log(this.inputEL); // Verificando se a instância do NgModel foi corretamente injetada
+  }
+
+  // Função chamada ao clicar no botão Enviar
+  send(){
+    // Verifica se o campo é válido e se já foi tocado pelo usuário
+    if (this.inputEL.valid && this.inputEL.touched){
+      console.log("Enviado com sucesso"); // Mensagem exibida se o formulário estiver válido e tiver sido tocado
+    } else {
+      console.log("O formulário não está pronto para ser enviado"); // Mensagem caso o formulário não atenda aos critérios
+    }
   }
 }
