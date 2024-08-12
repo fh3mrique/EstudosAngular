5cb5d8bb3f30c0b647587c60ad8eaecf59474f7b
Filipe Lima <fhemrique55@gmail.com>
2024-08-12 09:44:08 -0300

    Desafio PARTE 3: Biding dos dados no display

--- 08 - PIPES/desafio/src/app/app.component.ts
+++ 08 - PIPES/desafio/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OperationsService } from './services/operations.service';
+import { OperationsListResponse } from './types/operations-list-response.type';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  operationsList: OperationsListResponse = [];
+
+  constructor(private readonly _operationsService: OperationsService) { }
+
+
+  ngOnInit() {
+    this.getOperations();
+  }
+
+  getOperations() {
+    this._operationsService.getOperations()
+    .pipe(take(1)) // simula um unsubscribe
+    .subscribe((operationsListRespponse) => {
+      this.operationsList = operationsListRespponse;
+    });
+
+    //para debugar se valores são chamados
+    setTimeout(() =>{
+      console.log(this.operationsList)
+    }, 3000)
+  }
+
 }
