24da807bf2b5810978a4c3b57229a9f9396a2a40
Filipe Lima <fhemrique55@gmail.com>
2024-08-13 07:29:27 -0300

    Diretiva NgModel com Elemento Radio Button

--- 10 - TEMPLATE DRIVEN FORMS/driven-forms/src/app/app.module.ts
+++ 10 - TEMPLATE DRIVEN FORMS/driven-forms/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FormsModule } from '@angular/forms';
 import { TextareaComponent } from './elements/textarea/textarea.component';
 import { InputTextComponent } from './elements/input-text/input-text.component';
 import { SelectComponent } from './elements/select/select.component';
+import { RadioButtonComponent } from './elements/radio-button/radio-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextareaComponent,
     InputTextComponent,
-    SelectComponent
+    SelectComponent,
+    RadioButtonComponent
   ],
   imports: [
     BrowserModule,
