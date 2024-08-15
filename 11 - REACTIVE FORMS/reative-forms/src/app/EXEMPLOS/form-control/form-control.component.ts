60cddce694202617db540eb17ce1759256eeb4d3
Filipe Lima <fhemrique55@gmail.com>
2024-08-15 16:35:37 -0300

    Como Observar Mudanças em um FormControl

--- 11 - REACTIVE FORMS/reative-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
+++ 11 - REACTIVE FORMS/reative-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
@@ -8,10 +8,15 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class FormControlComponent implements OnInit {
     
-    nome = new FormControl({value: '', disabled: true}, [Validators.required, Validators.minLength(6)])
+    //nome = new FormControl({value: '', disabled: true}, [Validators.required, Validators.minLength(6)])
+    nome = new FormControl('Inicial', {nonNullable: true, validators: [Validators.required], updateOn: 'blur' })
 
     ngOnInit(): void {
       console.log(this.nome)
+
+      this.nome.valueChanges.subscribe(result => console.log('ValueChanges: ', result))
+
+      this.nome.statusChanges.subscribe(result => console.log('statusChanges: ', result))
     }
 
     mostrarStatus(){
