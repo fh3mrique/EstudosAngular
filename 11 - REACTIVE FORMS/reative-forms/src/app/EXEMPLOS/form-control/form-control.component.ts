b9b37dab0875780d189533ead2b4412318452809
Filipe Lima <fhemrique55@gmail.com>
2024-08-15 08:38:59 -0300

    Como Criar um FormControl

--- 11 - REACTIVE FORMS/reative-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
+++ 11 - REACTIVE FORMS/reative-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control',
   templateUrl: './form-control.component.html',
   styleUrls: ['./form-control.component.css']
 })
-export class FormControlComponent {
+export class FormControlComponent implements OnInit {
+    
+    nome = new FormControl('Valor inicial', [Validators.required])
 
+    ngOnInit(): void {
+      console.log(this.nome)
+    }
+
+    mostrarStatus(){
+      console.log(this.nome)
+    }
 }
