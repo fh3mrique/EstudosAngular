62680d6e3113c042e4a5f17f91a84b362dbd269a
Filipe Lima <fhemrique55@gmail.com>
2024-08-15 08:29:20 -0300

    Criação do Projeto e Configuração do Ambiente

---
+++ 11 - REACTIVE FORMS/reative-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control',
+  templateUrl: './form-control.component.html',
+  styleUrls: ['./form-control.component.css']
+})
+export class FormControlComponent {
+
+}
--- 11 - REACTIVE FORMS/reative-forms/src/app/app.module.ts
+++ 11 - REACTIVE FORMS/reative-forms/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormControlComponent } from './EXEMPLOS/form-control/form-control.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormControlComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    /* Precisa ter o modulo de ReactiveFormsModule para
+    as diretivass do reactive forms funcionarem */
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
