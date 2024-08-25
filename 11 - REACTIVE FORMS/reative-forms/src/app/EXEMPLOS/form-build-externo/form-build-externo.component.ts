b2be719f3c5e6ba8cd160477a8df2b7091cce38a
Filipe Lima <fhemrique55@gmail.com>
2024-08-25 15:08:49 -0300

    Implementação da Classe Controller no Template

--- 11 - REACTIVE FORMS/reative-forms/src/app/EXEMPLOS/form-build-externo/form-build-externo.component.ts
+++ 11 - REACTIVE FORMS/reative-forms/src/app/EXEMPLOS/form-build-externo/form-build-externo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { PessoaFormController } from './pessoa-form-controller';
 import { FormBuilder } from '@angular/forms';
 
@@ -8,10 +8,15 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./form-build-externo.component.css']
 })
 export class FormBuildExternoComponent extends PessoaFormController{
-  
+  @Output('OnFormSubmit') onFormSubmitEmit = new EventEmitter();
+
   constructor(private readonly _fbMain: FormBuilder){
     super(_fbMain)
   }
 
-  
+  onFormSubmit(){
+    console.log('onFormSubmit => ', this.pessoaForm.value);
+
+    this.onFormSubmitEmit.emit(this.pessoaForm.value)
+  }
 }
