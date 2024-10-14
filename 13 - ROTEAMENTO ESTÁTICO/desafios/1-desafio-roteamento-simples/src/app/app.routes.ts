d2ac651666f305a2cae7c9b36a29adc3770a9cab
Filipe Lima <fhemrique55@gmail.com>
2024-10-13 23:00:43 -0300

    Desafio 1: Resolução

--- 13 - ROTEAMENTO ESTÁTICO/desafios/1-desafio-roteamento-simples/src/app/app.routes.ts
+++ 13 - ROTEAMENTO ESTÁTICO/desafios/1-desafio-roteamento-simples/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'informations',
+        loadComponent: () => import('./components/informations/informations.component').then(m => m.InformationsComponent)
+    },
+    {
+        path: 'contacts',
+        loadComponent: () => import('./components/contacts/contacts.component').then(m => m.ContactsComponent)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent)
+    },
+    {
+        path: 'exit',
+        loadComponent: () => import('./components/exit/exit.component').then(m => m.ExitComponent)
+    }
+];
