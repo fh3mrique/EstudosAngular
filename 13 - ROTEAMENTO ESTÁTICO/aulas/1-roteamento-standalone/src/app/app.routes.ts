e5294b321ad857060b583a49e15db6d8bd927160
Filipe Lima <fhemrique55@gmail.com>
2024-10-13 22:10:10 -0300

    Carregamento por Demanda com o Lazy Loading

--- 13 - ROTEAMENTO ESTÁTICO/aulas/1-roteamento-standalone/src/app/app.routes.ts
+++ 13 - ROTEAMENTO ESTÁTICO/aulas/1-roteamento-standalone/src/app/app.routes.ts
@@ -8,10 +8,24 @@ import { BaseComponent } from './components/base/base.component';
     {path: 'segundo', component: SegundoComponent},
 ]; */
 
-export const routes: Routes = [
+/* export const routes: Routes = [
     {path: 'componentes', component: BaseComponent},
     {path: 'componentes/primeiro', component: PrimeiroComponent},
     {path: 'componentes/segundo', component: SegundoComponent},
+]; */
+export const routes: Routes = [
+    {
+        path: 'componentes',
+        loadComponent: () => import('./components/base/base.component').then(m => m.BaseComponent)
+    },
+    {
+        path: 'componentes/primeiro',
+        loadComponent: () => import('./components/primeiro/primeiro.component').then(m => m.PrimeiroComponent)
+    },
+    {
+        path: 'componentes/segundo',
+        loadComponent: () => import('./components/segundo/segundo.component').then(m => m.SegundoComponent)
+    },
 ];
 
 
