625c63593aa65128f1053b132d0bb5236527ba88
Filipe Lima <fhemrique55@gmail.com>
2024-10-21 09:27:33 -0300

    Resolução do Desafio 1 Parte 2: Criação do UsersListService

---
+++ 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/services/users-list.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { UsersListResponse } from "../types/users-list-response";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UsersListService {
+    private readonly _http = inject(HttpClient);
+
+    getUsers(): Observable<UsersListResponse> {
+        return this._http.get<UsersListResponse>('https://jsonplaceholder.typicode.com/users')
+    }
+}
+
+
