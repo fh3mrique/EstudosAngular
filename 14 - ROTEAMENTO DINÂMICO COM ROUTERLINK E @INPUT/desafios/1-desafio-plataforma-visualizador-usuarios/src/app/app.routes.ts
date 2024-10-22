da34e548a9619cd9661b924eccf809ffe79ee9b8
Filipe Lima <fhemrique55@gmail.com>
2024-10-22 17:11:56 -0300

    Resolução do Desafio 1 Parte 9: Criação da Rota de Lista de Usuários

--- 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/app.routes.ts
+++ 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/users-list',
+        pathMatch: 'full'
+    },
+    {
+        path: 'users-list',
+        title: 'Lista de usuarios',
+        component: UsersListComponent
+    },
     {
         path: '**',
         title: 'Pagina não encontrada',
