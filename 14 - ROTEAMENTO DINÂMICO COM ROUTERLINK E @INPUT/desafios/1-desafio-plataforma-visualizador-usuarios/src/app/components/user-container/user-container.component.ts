3e79c4e0b10570a29c74ee7a755582b42849a917
Filipe Lima <fhemrique55@gmail.com>
2024-10-22 17:30:46 -0300

    Resolução do Desafio 1 Parte 11: Recuperando os Dados do Usuário Selecionado

--- 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/user-container.component.ts
+++ 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/user-container.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IUser } from '../../interfaces/user.interface';
+import { UsersListService } from '../../services/users-list.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-container',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './user-container.component.html',
   styleUrl: './user-container.component.scss'
 })
 export class UserContainerComponent {
+  user$ : Observable<IUser> = of({} as IUser);
+  
+  private readonly _userListService =  inject(UsersListService);
 
+  @Input() set userId(value: string) {
+    this.user$ = this._userListService.getUser(value);
+  }
 }
