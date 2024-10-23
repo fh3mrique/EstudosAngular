e2b3a99d1972e4ad7c322da8539993d7a661588c
Filipe Lima <fhemrique55@gmail.com>
2024-10-23 16:32:08 -0300

    Resolução do Desafio 1 Parte 12: Criação e Redirecionamento das Rotas Filhas

--- 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/user-container.component.ts
+++ 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/user-container.component.ts
@@ -3,11 +3,16 @@ import { Observable, of } from 'rxjs';
 import { IUser } from '../../interfaces/user.interface';
 import { UsersListService } from '../../services/users-list.service';
 import { AsyncPipe } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-container',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [
+    AsyncPipe, 
+    RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive],
   templateUrl: './user-container.component.html',
   styleUrl: './user-container.component.scss'
 })
