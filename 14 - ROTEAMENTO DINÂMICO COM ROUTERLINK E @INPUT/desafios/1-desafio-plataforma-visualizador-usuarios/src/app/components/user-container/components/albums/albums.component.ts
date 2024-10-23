e73d618b75f95cd856d74c2b2d4a9068fe10737c
Filipe Lima <fhemrique55@gmail.com>
2024-10-23 17:00:33 -0300

    Resolução do Desafio 1 Parte 14: Carregamento da Lista de Albums do Usuário

--- 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/components/albums/albums.component.ts
+++ 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/components/albums/albums.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { AlbumsListResponse } from '../../../../types/albums-list-response';
+import { ActivatedRoute } from '@angular/router';
+import { AmbumsListService } from '../../../../services/albums-list.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.scss'
 })
-export class AlbumsComponent {
+export class AlbumsComponent implements OnInit {
+ 
+  albumsList$: Observable<AlbumsListResponse> = of([]);
+
+  private readonly _activatedRouter = inject(ActivatedRoute);
+  private readonly _albumsService = inject(AmbumsListService);
+
+  ngOnInit() {
+    this._activatedRouter.parent?.params.subscribe(
+      (params) => this.albumsList$ = this._albumsService.getUsersAlbums(params['userId'])
+    )
+
+  }
 
 }
