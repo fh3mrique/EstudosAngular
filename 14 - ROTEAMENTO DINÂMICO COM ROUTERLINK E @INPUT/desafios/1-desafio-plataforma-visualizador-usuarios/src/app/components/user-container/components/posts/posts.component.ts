dcb5af1031a01b736c76d5dc7594d4a7b42e5699
Filipe Lima <fhemrique55@gmail.com>
2024-10-23 17:06:46 -0300

    Resolução do Desafio 1 Parte 15: Carregamento da Lista de Posts do Usuário

--- 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/components/posts/posts.component.ts
+++ 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/components/posts/posts.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { PostListResponse } from '../../../../types/posts-list-response';
+import { ActivatedRoute } from '@angular/router';
+import { PostListService } from '../../../../services/posts-list.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+
+  postsList$: Observable<PostListResponse> = of([]);
+
+  private readonly _activatedRouter = inject(ActivatedRoute);
+  private readonly _postsService = inject(PostListService);
+
+  ngOnInit() {
+    this._activatedRouter.parent?.params.subscribe(
+      (params) => this.postsList$ = this._postsService.getUserPost(params['userId'])
+    )
+
+  }
 
 }
