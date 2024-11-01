1a9d4477694304e583a42c5613b0a646dadc602a
Filipe Lima <fhemrique55@gmail.com>
2024-11-01 20:39:22 -0300

    Resolução do Desafio 1 Parte 17: Recuperando os Dados do Post Selecionado

--- 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/components/post/post.component.ts
+++ 14 - ROTEAMENTO DINÂMICO COM ROUTERLINK E @INPUT/desafios/1-desafio-plataforma-visualizador-usuarios/src/app/components/user-container/components/post/post.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IPost } from '../../../../interfaces/post.interface';
+import { PostListService } from '../../../../services/posts-list.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
+
+  post$: Observable<IPost> = of({} as IPost);
+
+  private readonly _postListService = inject(PostListService);
+
+
+  @Input() set postId(value: string) {
+    this.post$ = this._postListService.getPost(value);
+  }
 
 }
